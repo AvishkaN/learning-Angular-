d47c28c733a7df5016d71993e5dc407069e738c5
TjTharuka <tharuka@speralabs.com>
2022-10-26 13:09:47 -0500

    -work

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,EventEmitter, Output, ChangeDetectionStrategy, SimpleChanges } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class RoomsListComponent implements OnInit {
 
   @Input() rooms:RoomList[]=[];
+  @Input() title:string='';
+
+  @Output() greetEvent =new EventEmitter();
+
+  name ='av';
 
   constructor() { }
 
+  ngOnChanges(changes:SimpleChanges){
+    console.log(changes);
+  }
+
   ngOnInit(): void {
+  }
+
+ 
+
+  callParent(room:RoomList){
+    this.greetEvent.emit(room);
   }
 
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild ,AfterViewInit} from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 import { Room, RoomList } from './rooms';
 
 @Component({
@@ -6,7 +7,7 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit,AfterViewInit{
 
   hotelName='hilton hotel';
   hotelRomms=25;
@@ -18,10 +19,23 @@ export class RoomsComponent implements OnInit {
     bookedRooms:15,
   }
   roomList : RoomList[]=[]
+  selectedRoom!: RoomList;
+
+  title='Room List'
+
+  @ViewChild(HeaderComponent) headerCompenent!:HeaderComponent;
+
 
   constructor() { }
 
+  // ngDoCheck(){
+  //   console.log('do check ...');  // it rund every change (its not good always avoid)
+  // }
+
   ngOnInit(): void {
+
+    console.log('Without ng view init',this.headerCompenent);
+
     this.roomList=[
       {
         roomNumber:29,  
@@ -71,8 +85,34 @@ export class RoomsComponent implements OnInit {
     ]
   }
 
+  ngAfterViewInit(){
+    console.log('INSIDE - ng view init ',this.headerCompenent);
+  }
+
   toggle(){
     this.hideRooms=!this.hideRooms;
+    this.title='New Title';
+  }
+
+  selectRoom(room:RoomList){
+    console.log('hello parent',room);
+    this.selectedRoom=room;
+  }
+
+  addRoom(){
+    const room:RoomList={
+      roomNumber:50,  
+      roomType:'Deluxe Room 50',
+      amenities:'Air conditioner, Free wie',
+      price:6000,
+      photos:'https://images.luxuryhotel.guru/hotelimage.php?p_id=4171893&code=0a59b6cd71094ff862ae6b57ccae75d1&webpage=amazing-hotel-suites.com&link=https%3A%2F%2Fsubdomain.cloudimg.io%2Fcrop%2F512x384%2Fq70.fcontrast10.fbright0.fsharp5%2Fhttps%3A%2F%2Fq-xx.bstatic.com%2Fxdata%2Fimages%2Fhotel%2Fmax1536%2F185181086.jpg%3Fk%3Da12281d2c6f7bc28fa39129e0b1b20601d791e38860bff0f4b84820f249f5f77%26o%3D',
+      checkinTime:new Date(),
+      checkoutTime:new Date('12-Nov-221')
+    };
+
+    // this.roomList.push(room);
+
+    this.roomList=[...this.roomList,room]
   }
 
 }
