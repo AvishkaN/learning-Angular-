c1b848aaf8b707c845e64c04bcf788d676817290
AvishkaN <avishkanirmitha@gmail.com>
2023-02-11 22:56:46 +0530

    Different ways of Creating Observable

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
 import { LogServiceService } from './services/log-service.service';
 import { SchoolServiceService } from './services/school-service.service';
 
@@ -13,7 +14,44 @@ export class SchoolComponent implements OnInit {
 
   classes: any;
 
+  arr1 = [1, 2, 3, 4, 5, 6];
+  arr2 = [7, 8, 9, 10];
+
+  // myObserver = new Observable((observer) => {
+  //   observer.next(1);
+  //   observer.next(2);
+  //   observer.next(3);
+  //   observer.next(4);
+  //   observer.error(new Error('something went wrong 💣💣 '));
+  //   observer.next(5);
+  // });
+
+  // myObserver = Observable.create((observer: any) => {
+  //   observer.next(1);
+  //   observer.next(2);
+  //   observer.next(3);
+  //   observer.next(4);
+  //   observer.error(new Error('something went wrong 💣💣 '));
+  //   observer.next(5);
+  // });
+
+  // myObserver = of(this.arr1, this.arr2);
+
+  myObserver = from(this.arr1);
+
   ngOnInit(): void {
     this.classes = this._schoolServiceService.classes;
+
+    this.myObserver.subscribe(
+      (value: any) => {
+        console.log(value);
+      },
+      (error: any) => {
+        alert(error.message);
+      },
+      () => {
+        console.log('👉 all values return successfully');
+      }
+    );
   }
 }
