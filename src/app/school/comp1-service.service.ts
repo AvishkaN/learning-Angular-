1a9636b070e4a009cba73e0b271b962804796db5
AvishkaN <avishkanirmitha@gmail.com>
2023-02-12 13:21:01 +0530

    data sharing in two components by using subject

--- src/app/school/comp1-service.service.ts
+++ src/app/school/comp1-service.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Comp1ServiceService {
-  dataEmiter = new EventEmitter<string>();
+  // dataEmiter = new EventEmitter<string>();
+  dataEmiter = new Subject<string>();
 
   constructor() {}
 
   ChangeServiceValue(data: string) {
-    this.dataEmiter.emit(data);
+    // this.dataEmiter.emit(data);
+    this.dataEmiter.next(data);
   }
 }
